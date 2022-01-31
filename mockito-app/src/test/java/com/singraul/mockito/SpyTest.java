7b35f82bfecaf574e0031382d6770d2809bae320
D51947403 <singraul20codeblog@gmail.com>
2022-01-31 18:24:12 +0530

    PowerMock for Static, private and constructor

--- mockito-app/src/test/java/com/singraul/mockito/SpyTest.java
+++ mockito-app/src/test/java/com/singraul/mockito/SpyTest.java
@@ -2,6 +2,8 @@ package com.singraul.mockito;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.stub;
+import static org.mockito.Mockito.verify;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,12 +17,39 @@ public class SpyTest {
 
 	@Test
 	public void test() {
-		List arryListSpy=spy(ArrayList.class);
+		List<String> arryListSpy=spy(ArrayList.class);
 		assertEquals(0, arryListSpy.size());
 		arryListSpy.add("Dummy" );
 		arryListSpy.add("Funny");
 		// updated actual object size
 		assertEquals(2, arryListSpy.size());
+	}
+
+	@Test
+	public void creatingASpyOnArrayList() {
+		List<String> listSpy = spy(ArrayList.class);
+		listSpy.add("Ranga");
+		listSpy.add("in28Minutes");
+
+		verify(listSpy).add("Ranga");
+		verify(listSpy).add("in28Minutes");
+
+		assertEquals(2, listSpy.size());
+		assertEquals("Ranga", listSpy.get(0));
+	}
+
+	@Test
+	public void creatingASpyOnArrayList_overridingSpecificMethods() {
+		List<String> listSpy = spy(ArrayList.class);
+		listSpy.add("Ranga");
+		listSpy.add("in28Minutes");
+
+		stub(listSpy.size()).toReturn(-1);
+
+		assertEquals(-1, listSpy.size());
+		assertEquals("Ranga", listSpy.get(0));
+
+		// @Spy Annotation
 	}
 
 }
